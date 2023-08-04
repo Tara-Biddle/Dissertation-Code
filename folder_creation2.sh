bb41b9b1176d1c3689ac2e2a606f407302628312
Tara Biddle <116201128+Tara-Biddle@users.noreply.github.com>
2023-08-04 13:48:00 +0100

    Update folder_creation2.sh

--- folder_creation2.sh
+++ folder_creation2.sh
@@ -1,9 +1,11 @@
 
-for i in {1..28}
+for i in {1..700}
 do
 newname="${i}" 
 mkdir $newname
+### Have all cell files numbered 1-700 in a folder 'cell_files' in the same location as this script
 cp cell_files/${i}.cell $newname/${newname}.cell
+### Have template param and sub files - template.param and template_sub - in a folder 'template' in the same location as this script
 cp template/*.param $newname/${newname}.param
 cp template/*sub $newname/${newname}_sub
 cd ${newname}
