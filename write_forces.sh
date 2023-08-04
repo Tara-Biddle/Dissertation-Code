e288cd898db16956b2a634c1f977f907a13c8983
Tara Biddle <116201128+Tara-Biddle@users.noreply.github.com>
2023-08-04 13:50:25 +0100

    Update write_forces.sh

--- write_forces.sh
+++ write_forces.sh
@@ -5,8 +5,6 @@ do
 cd ${i}
 
 coords=$(sed -n '310,311p;312q' ${i}.castep)
-#forces1=$(sed -n '/* H              18/,/*/p' ${i}.castep)
-#forces2=$(sed -n '/* H              19/,/*/p' ${i}.castep)
 forces1=$(awk '/* H              18/,/*/' ${i}.castep | head -1)
 forces2=$(awk '/* H              19/,/*/' ${i}.castep | head -1)
 writevar="${i} ${coords} ${forces1} ${forces2}"
